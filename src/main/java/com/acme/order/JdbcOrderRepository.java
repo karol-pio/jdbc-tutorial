fb62fe79476f3cf6d1c4e28c4722250be6ea079b
KWojtaszewski <karol.wojtaszewski@stazysta.comarch.pl>
2015-07-09 11:01:41 +0200

    Add impl in method findAll

--- src/main/java/com/acme/order/JdbcOrderRepository.java
+++ src/main/java/com/acme/order/JdbcOrderRepository.java
@@ -1,9 +1,17 @@
 package com.acme.order;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
+
+import javax.sql.DataSource;
 
 import lombok.extern.slf4j.Slf4j;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Repository;
 
@@ -20,6 +28,12 @@ public class JdbcOrderRepository implements OrderRepository {
 
 	private final String password = "dbpass";
 
+	final String SQL = "SELECT o.id as order_id,o.customer_id as customer_id,o.status,o.type,o.estimatedDeliveryTime,"
+			+ "o.finishTime,c.name,c.email,c.address from order_t o,customer_t c where o.customer_id = c.id";
+
+	@Autowired
+	private DataSource ds;
+	
 	@Override
 	public String save(PizzaOrder order) {
 		// TODO Auto-generated method stub
@@ -40,8 +54,75 @@ public class JdbcOrderRepository implements OrderRepository {
 
 	@Override
 	public List<PizzaOrder> findAll() {
-		// TODO Auto-generated method stub
+		Connection conn = null;
+		Statement stmt = null;
+		
+		try {
+
+			// STEP 3: Open a connection
+			System.out.println("Connecting to database...");
+//			conn = DriverManager.getConnection(url, user, password);
+			conn = ds.getConnection();
+			// STEP 4: Execute a query
+			System.out.println("Creating statement...");
+			stmt = conn.createStatement();
+			String sql;
+			sql = "SELECT id, customer_id, estimatedDeliveryTime, finishTime FROM order_t";
+			ResultSet rs = stmt.executeQuery(sql);
+
+			// STEP 5: Extract data from result set
+			while (rs.next()) {
+				
+//				PizzaOrder pizzaOrder = new PizzaOrder(customer, pizzaType)
+				// Retrieve by column name
+				int id = rs.getInt("id");
+				int age = rs.getInt("customer_id");
+				String first = rs.getString("estimatedDeliveryTime");
+				String last = rs.getString("finishTime");
+
+				// Display values
+				System.out.print("id: " + id);
+				System.out.print(", customer_id: " + age);
+				System.out.print(", estimatedDeliveryTime: " + first);
+				System.out.println(", finishTime: " + last);
+			}
+			// STEP 6: Clean-up environment
+			rs.close();
+			stmt.close();
+			conn.close();
+		} catch (
+
+		SQLException se)
+
+		{
+			// Handle errors for JDBC
+			se.printStackTrace();
+		} catch (
+
+		Exception e)
+
+		{
+			// Handle errors for Class.forName
+			e.printStackTrace();
+		} finally
+
+		{
+			// finally block used to close resources
+			try {
+				if (stmt != null)
+					stmt.close();
+			} catch (SQLException se2) {
+			} // nothing we can do
+			try {
+				if (conn != null)
+					conn.close();
+			} catch (SQLException se) {
+				se.printStackTrace();
+			} // end finally try
+		} // end try
+		System.out.println("GoodBye");
 		return null;
+
 	}
 
 	@Override
--- src/main/java/com/acme/order/application/SpringAnnotationBasedApplication.java
+++ src/main/java/com/acme/order/application/SpringAnnotationBasedApplication.java
@@ -2,14 +2,22 @@ package com.acme.order.application;
 
 import lombok.extern.slf4j.Slf4j;
 
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 import com.acme.order.Customer;
+import com.acme.order.JdbcOrderRepository;
+import com.acme.order.OrderRepository;
 import com.acme.order.pizza.PizzaOrderService;
 import com.acme.order.pizza.PizzaType;
+import com.mysql.jdbc.ConnectionProperties;
+import com.mysql.jdbc.ConnectionPropertiesImpl;
 
 @Slf4j
 @Configuration
@@ -25,18 +33,37 @@ public class SpringAnnotationBasedApplication {
 		Customer customer1 = new Customer(null, "John Smith", "john@smith.com", "Lodz, Jaracza 74");
 		Customer customer2 = new Customer(null, "Jan Kowalski", "jan@kowalski.pl", "Lodz, Piotrkowska 100");
 
-		String orderId1 = orderService.createOrder(customer1, PizzaType.LARGE);
-		String orderId2 = orderService.createOrder(customer2, PizzaType.SMALL);
-
-		log.info("Unprocessed orders:{}", orderService.fetchUnprocessed());
-		log.info("Delivered orders:{}", orderService.fetchDelivered());
-
-		orderService.deliverOrder(orderId1);
-		log.info("Delivered orders:{}", orderService.fetchDelivered());
-		orderService.cancelOrder(orderId2);
-		log.info("Delivered orders:{}", orderService.fetchDelivered());
-		log.info("Cancelled orders:{}", orderService.fetchCancelled());
-		log.info("Unprocessed orders:{}", orderService.fetchUnprocessed());
+		OrderRepository orderRepository = ctx.getBean(JdbcOrderRepository.class);
+		
+		orderRepository.findAll();
+//
+//		String orderId1 = orderService.createOrder(customer1, PizzaType.LARGE);
+//		String orderId2 = orderService.createOrder(customer2, PizzaType.SMALL);
+//
+//		log.info("Unprocessed orders:{}", orderService.fetchUnprocessed());
+//		log.info("Delivered orders:{}", orderService.fetchDelivered());
+//
+//		orderService.deliverOrder(orderId1);
+//		log.info("Delivered orders:{}", orderService.fetchDelivered());
+//		orderService.cancelOrder(orderId2);
+//		log.info("Delivered orders:{}", orderService.fetchDelivered());
+//		log.info("Cancelled orders:{}", orderService.fetchCancelled());
+//		log.info("Unprocessed orders:{}", orderService.fetchUnprocessed());
+	}
+	
+	@Bean(destroyMethod = "close")
+	public DataSource ds() {
+		BasicDataSource ds = new BasicDataSource();
+		ds.setDriverClassName("com.mysql.jdbc.Driver");
+		ds.setUrl("jdbc:mysql://localhost:3306/pizza-tutorial");
+		ds.setUsername("dbuser");
+		ds.setPassword("dbpass");
+		
+		ds.setInitialSize(5);
+		
+		
+//		ds.setConnectionProperties("initialSize=5");
+		return ds;
 	}
 
 }
